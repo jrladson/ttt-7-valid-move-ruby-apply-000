11097c79e7fc4b1a61b641a835e97e49d82c82a7
JR Ladson <jrladson@gmail.com>
2020-11-25 04:18:49 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,7 @@
-def position_taken?(board, index)
-  if board[index] = " " || board[index] = "" || board[index] = NIL
-    return FALSE
-  else
-    return TRUE
-  end
-end
+
 
 def valid_move?(board, index)
-  if board[index] && index.between?(0,8)
+  if !position_taken?board[index] && index.between?(0,8)
     retrun TRUE
   else
     FALSE
